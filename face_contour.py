0a5cc3955c412419e8d55da999b20e9725ade03d
Nitish Chhabra <chhab798@gmail.com>
2020-09-26 20:31:38 +0530

    added mouth tracking as well and changed jump criteria from blink to mouth opening

--- face_contour.py
+++ face_contour.py
@@ -21,18 +21,31 @@ def eye_aspect_ratio(eye):
     # return the eye aspect ratio
     return ear
 
+def mouth_aspect_ration(inner_mouth):
+    # print("Entered MAR")
+    # Compute the euclidean distances between the two sets of
+    # vertical inner mouth landmarks (x, y)-coordinates
+    A = dist.euclidean(inner_mouth[2], inner_mouth[6])
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    B = dist.euclidean(inner_mouth[0], inner_mouth[4])
+    # compute the eye aspect ratio
+    mar = A / B
+    # return the eye aspect ratio
+    return mar
+
 def eye_to_eyebrow_distance(eye, eyebrow):
     # print("Entered Eye to Eyebrow")
     # compute distance of 3rd and 4th point in eyebrow
     # with 2nd and 3rd point in the eye
-    A = dist.euclidean(eye[1], eyebrow[2])
-    B = dist.euclidean(eye[2], eyebrow[3])
+    A = dist.euclidean(eye[0], eyebrow[0])
+    B = dist.euclidean(eye[3], eyebrow[4])
     # compute the eye aspect ratio
     eye_to_eyebrow_distance = (A + B) / (2.0)
     # return the eye to eyebrow distance
     return eye_to_eyebrow_distance
 
-def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL, EYE_AR_THRESH, EYE_AR_CONSEC_FRAMES, EYEBROW_DIST_THRESH, draw = 1):
+def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL_BLINKS, EYE_AR_THRESH, CONSECUTIVE_FRAMES, EYEBROW_DIST_THRESH, MAR_COUNTER, MAR_THRESH, draw = 1):
     # print("Entered detect landmarks")
 
     function_frame = input_frame.copy()
@@ -61,7 +74,8 @@ def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNT
         (reStart, reEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
         (lebStart, lebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
         (rebStart, rebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
-
+        (mStart, mebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["inner_mouth"]
+        
         # Helper Information on how to use the above dictionary:
         # Face utils helps in extracting various face features without need to remember id locations for each feature
         # This dictionary gives guidance on which all Landmarks can be detected
@@ -93,6 +107,9 @@ def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNT
         rightEyeToEyebrow = eye_to_eyebrow_distance(rightEye, rightEyeBrow)
         # average the eye aspect ratio together for both eyes
         ete_dist = (leftEyeToEyebrow + rightEyeToEyebrow) / 2.0
+
+        inner_mouth = rightEyeBrow = shape[mStart:mebEnd]
+        mar = mouth_aspect_ration(inner_mouth)
  
         # compute the convex hull for the left and right eye, then
         # visualize each of the eyes
@@ -106,7 +123,7 @@ def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNT
             #Don't draw anything
             pass
         
-        decisions = {"blinked" : 0, "browed" : 0}
+        decisions = {"blinked" : 0, "browed" : 0, "mouth_opened" : 0}
         # check to see if the eye aspect ratio is below the blink
         # threshold, and if so, increment the blink frame counter
         if ear < EYE_AR_THRESH:
@@ -115,36 +132,49 @@ def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNT
         # threshold
         else:
             # if the eyes were closed for a sufficient number of frames
-            # then increment the total number of blinks
-            if BLINK_COUNTER >= EYE_AR_CONSEC_FRAMES:
-                TOTAL += 1
+            # then increment the TOTAL_BLINKS number of blinks
+            if BLINK_COUNTER >= CONSECUTIVE_FRAMES:
+                TOTAL_BLINKS += 1
                 decisions["blinked"] = 1
             # reset the eye frame counter
                 BLINK_COUNTER = 0
         
         if ete_dist > EYEBROW_DIST_THRESH:
             BROW_COUNTER += 1
-            if BROW_COUNTER >= EYE_AR_CONSEC_FRAMES:
+            if BROW_COUNTER >= CONSECUTIVE_FRAMES:
                 decisions["browed"] = 1
             else:
                 pass
         else:
             BROW_COUNTER = 0
 
+        if mar > MAR_THRESH:
+            MAR_COUNTER += 1
+            if MAR_COUNTER >= CONSECUTIVE_FRAMES:
+                decisions["mouth_opened"] = 1
+            else:
+                pass
+        else:
+            MAR_COUNTER = 0
 
-        # draw the total number of blinks on the frame along with
+
+        # draw the TOTAL_BLINKS number of blinks on the frame along with
         # the computed eye aspect ratio for the frame
         if draw:
-            cv2.putText(function_frame, "Blinks: {}".format(TOTAL), (10, 30),
+            cv2.putText(function_frame, "Blinks: {}".format(TOTAL_BLINKS), (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.putText(function_frame, "EAR: {:.2f}".format(ear), (300, 30),
+            cv2.putText(function_frame, "EAR: {:.2f}".format(ear), (250, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.putText(function_frame, "BD: {:.2f}".format(ete_dist), (420, 30),
+            cv2.putText(function_frame, "BD: {:.2f}".format(ete_dist), (370, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            cv2.putText(function_frame, "In Blink: {:.2f}".format(1 if BLINK_COUNTER > EYE_AR_CONSEC_FRAMES else 0), (10, 450),
+            cv2.putText(function_frame, "MAR: {:.2f}".format(mar), (500, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)    
+            cv2.putText(function_frame, "In Blink: {:.2f}".format(1 if BLINK_COUNTER > CONSECUTIVE_FRAMES else 0), (10, 450),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             cv2.putText(function_frame, "In Brow: {:.2f}".format(decisions["browed"]), (200, 450),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(function_frame, "Mouth Open: {:.2f}".format(decisions["mouth_opened"]), (400, 450),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)    
 
             # We are then accesing the landmark points  
             for n in range(0, 68): 
@@ -157,20 +187,22 @@ def detect_landmarks(input_frame, face_detector, landmark_predictor, BLINK_COUNT
             pass
 
     if draw:
-        return function_frame, decisions, BLINK_COUNTER, BROW_COUNTER , TOTAL
+        return function_frame, decisions, BLINK_COUNTER, BROW_COUNTER, MAR_COUNTER , TOTAL_BLINKS
     else:
-        return decisions, BLINK_COUNTER, BROW_COUNTER, TOTAL
+        return decisions, BLINK_COUNTER, BROW_COUNTER, MAR_COUNTER, TOTAL_BLINKS
 
 if __name__ == "__main__":
 
-    # initialize the frame counters and the total number of blinks
+    # initialize the frame counters and the TOTAL_BLINKS number of blinks
     EYE_AR_THRESH = FaceTrackingConstants.EYE_AR_THRESH #0.27
     EYEBROW_DIST_THRESH = FaceTrackingConstants.EYEBROW_DIST_THRESH #0.1
-    EYE_AR_CONSEC_FRAMES = FaceTrackingConstants.EYE_AR_CONSEC_FRAMES #2
+    MAR_THRESH = FaceTrackingConstants.MAR_THRESH
+    CONSECUTIVE_FRAMES = FaceTrackingConstants.CONSECUTIVE_FRAMES #2
 
     BLINK_COUNTER = FaceTrackingConstants.BLINK_COUNTER #0
     BROW_COUNTER = FaceTrackingConstants.BROW_COUNTER #0
-    TOTAL = FaceTrackingConstants.TOTAL #0
+    MAR_COUNTER = FaceTrackingConstants.MAR_COUNTER #0
+    TOTAL_BLINKS = FaceTrackingConstants.TOTAL_BLINKS #0
 
     cap = cv2.VideoCapture(0) 
     # We initialise detector of dlib 
@@ -183,10 +215,10 @@ if __name__ == "__main__":
 
         if got_frame:
             start_time = time.time()
-            output_frame, BLINK_COUNTER, BROW_COUNTER, TOTAL = detect_landmarks(frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL, EYE_AR_THRESH, EYE_AR_CONSEC_FRAMES, EYEBROW_DIST_THRESH)
+            output_frame, _, BLINK_COUNTER, BROW_COUNTER, MAR_COUNTER, TOTAL_BLINKS = detect_landmarks(frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL_BLINKS, EYE_AR_THRESH, CONSECUTIVE_FRAMES, EYEBROW_DIST_THRESH, MAR_COUNTER, MAR_THRESH)
             end_time = time.time()
             fps = round(1/(end_time-start_time),2)
-            cv2.putText(output_frame, "FPS: {}".format(fps), (150, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(output_frame, "FPS: {}".format(fps), (120, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             # output_frame = cv2.resize(output_frame,(1920,1080),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
             stack = np.hstack([frame,output_frame])
             cv2.imshow("Frame", stack) 
--- movement_control.py
+++ movement_control.py
@@ -13,16 +13,20 @@ import time
 # initialize the frame counters and the total number of blinks
 EYE_AR_THRESH = FaceTrackingConstants.EYE_AR_THRESH #0.27
 EYEBROW_DIST_THRESH = FaceTrackingConstants.EYEBROW_DIST_THRESH #0.1
-EYE_AR_CONSEC_FRAMES = FaceTrackingConstants.EYE_AR_CONSEC_FRAMES #2
+MAR_THRESH = FaceTrackingConstants.MAR_THRESH
+CONSECUTIVE_FRAMES = FaceTrackingConstants.CONSECUTIVE_FRAMES #2
 
 BLINK_COUNTER = FaceTrackingConstants.BLINK_COUNTER #0
 BROW_COUNTER = FaceTrackingConstants.BROW_COUNTER #0
-TOTAL = FaceTrackingConstants.TOTAL #0
+MAR_COUNTER = FaceTrackingConstants.MAR_COUNTER #0
+TOTAL_BLINKS = FaceTrackingConstants.TOTAL_BLINKS #0
 
 cap = cv2.VideoCapture(0) 
 # We initialise detector of dlib 
 face_detector = dlib.get_frontal_face_detector() 
 landmark_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") 
+
+pyautogui.PAUSE = 0.0
 
 # Start the main program 
 while(cap.isOpened()): 
@@ -30,29 +34,34 @@ while(cap.isOpened()):
 
     if got_frame:
         start_time = time.time()
-        function_frame, decisions, BLINK_COUNTER, BROW_COUNTER, TOTAL = detect_landmarks(frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL, EYE_AR_THRESH, EYE_AR_CONSEC_FRAMES, EYEBROW_DIST_THRESH, draw=1)
-        # print(decisions, BLINK_COUNTER)
+        # function_frame, decisions, BLINK_COUNTER, BROW_COUNTER, MAR_COUNTER, TOTAL_BLINKS = detect_landmarks(frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL_BLINKS, EYE_AR_THRESH, CONSECUTIVE_FRAMES, EYEBROW_DIST_THRESH, MAR_COUNTER, MAR_THRESH, draw=1)
+        decisions, BLINK_COUNTER, BROW_COUNTER, MAR_COUNTER, TOTAL_BLINKS = detect_landmarks(frame, face_detector, landmark_predictor, BLINK_COUNTER, BROW_COUNTER, TOTAL_BLINKS, EYE_AR_THRESH, CONSECUTIVE_FRAMES, EYEBROW_DIST_THRESH, MAR_COUNTER, MAR_THRESH, draw=0)
+        print(decisions)
+
         try:
-            if decisions["browed"] == 1:
-                pyautogui.press('down', pause = 0)
-             
-            elif decisions["blinked"] == 1:
-                pyautogui.press('space', pause = 0)
+            if decisions["mouth_opened"] == 1:
+                print("Pressing space")
+                pyautogui.press('space')
+
+            elif decisions["browed"] == 1:
+                print("Pressing down")
+                pyautogui.keydown('down')
+                pyautogui.keyup('down')
                 # pass
-        except: 
-            pass
+        except Exception as e:
+            print(e)
 
-        end_time = time.time()
-        fps = round(1/(end_time-start_time),2)
-        cv2.putText(function_frame, "FPS: {}".format(fps), (150, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        # end_time = time.time()
+        # fps = round(1/(end_time-start_time),2)
+        # cv2.putText(function_frame, "FPS: {}".format(fps), (150, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-        cv2.imshow("Frame", function_frame) 
-        key = cv2.waitKey(1) 
-        if key == 27: 
-            break # press esc the frame is destroyed
+        # cv2.imshow("Frame", function_frame) 
+        # key = cv2.waitKey(1) 
+        # if key == 27: 
+        #     break # press esc the frame is destroyed
 
-        # if keyboard.is_pressed('esc'):  # if key 'q' is pressed 
-        #     break  # finishing the loop
+        if keyboard.is_pressed('esc'):  # if key 'q' is pressed 
+            break  # finishing the loop
   
 cv2.destroyAllWindows()
 cap.release()   
